f0e51f8f5d73ecef46cc4cbe5fe5dde0e2bd3355
nhn29 <nhn29@drexel.edu>
2017-07-18 03:15:23 -0400

    Set up node server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import Express from 'express';
+import path from 'path';
+
+const app = new Express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(Express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'development';
+
+app.listen(port, err => {
+    console.log("Server is listening on port", port);
+});
