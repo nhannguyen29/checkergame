80175283faa1d4c1fcdc94eb31f5c072335c5ec6
PhucNgo1711 <phucnmrot@gmail.com>
2017-08-28 19:30:46 -0400

    List UI update

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -12,7 +12,7 @@ export default class Log extends React.Component {
                     </Card.Header>
                 </Card.Content>
                 <Card.Content className="logList" style={{ overflowY: 'auto' }}>
-                    <List items={logItems.map((item, i) => ({ content: item, key: i }))} />
+                    <List relaxed divided items={logItems.map((item, i) => ({ content: item, key: i }))} />
                 </Card.Content>
             </Card>
         );
